196921e41461ba47d67952b38eb0919b4e9b31f2
krishnakp2003 <krishnakumarprajapati2003@gmail.com>
2025-05-08 20:55:18 +0530

    Basic Project setup

---
+++ src/main/java/com/lost/found/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.lost.found.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	public UserController() {
+		System.out.println("UserController:Object Created");
+	}
+}
