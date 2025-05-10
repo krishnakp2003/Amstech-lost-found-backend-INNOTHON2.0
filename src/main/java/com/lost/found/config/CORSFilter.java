e0ca9ab65d20b764caeefcdddc95c24c1209dec4
krishnakp2003 <krishnakumarprajapati2003@gmail.com>
2025-05-10 10:35:44 +0530

    configuration

--- src/main/java/com/lost/found/config/CORSFilter.java
+++ src/main/java/com/lost/found/config/CORSFilter.java
@@ -1,47 +1,23 @@
 package com.lost.found.config;
 
-import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public class CORSFilter implements Filter{
-	public CORSFilter() {
-		System.out.println("CORSFilter: Object created..");
+@Configuration
+public class CORSFilter {
+	    @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/") // allow all routes
+	                        .allowedOrigins("http://localhost:4200") // allow Angular
+	                        .allowedMethods("POST", "PUT", "DELETE", "OPTIONS")
+	                        .allowedHeaders("*")
+	                        .allowCredentials(true);
+	            }
+	        };
+	    }
 	}
-
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-
-		// CORS Headers
-		response.setHeader("Access-Control-Allow-Origin", "*"); 
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "*");
-
-		// Pre-flight Requests Handle
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
-			return;
-		}
-
-		chain.doFilter(req, res);
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) {
-	}
-
-	@Override
-	public void destroy() {
-	}
-}
